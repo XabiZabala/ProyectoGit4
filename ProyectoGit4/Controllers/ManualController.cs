fada794dd1c699a602b74590009c7d0a98da23b7
xabi123 <javier7422@gmail.com>
2022-03-04 11:13:48 +0100

    Creados nuevos enlaces para manual y video

---
+++ ProyectoGit4/Controllers/ManualController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoGit4.Controllers
+{
+    public class ManualController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Servicios()
+        {
+            return View();
+        }
+
+
+    }
+}
