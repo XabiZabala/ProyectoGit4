a36585de798b52dec667080fe79c44c5954b2695
xabi123 <javier7422@gmail.com>
2022-03-07 11:25:25 +0100

    Creados nuevos enlaces a controlador Directivos y vistas de Directivos Index y Ver

---
+++ ProyectoGit4/Controllers/DirectivosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoGit4.Controllers
+{
+    public class DirectivosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ver()
+        {
+            return View();
+        }
+    }
+}
